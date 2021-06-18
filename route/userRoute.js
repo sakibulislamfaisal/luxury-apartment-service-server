e64578cdafce05616de82758477f191bf7c2f89f
sakibulislamfaisal <foyshalrahmanseu@gmail.com>
2021-06-18 15:02:17 +0600

    some file added and config file updated

--- route/userRoute.js
+++ route/userRoute.js
@@ -9,7 +9,7 @@ router.post("/signup", userController.createUser);
 router.post("/login", userController.login);
 router.get("/users", userController.getAllUsers);
 router.get("/:id", userController.singleUser);
-router.get("/email/:email", checkLogin, userController.getUserByEmail);
+router.get("/email/:email", userController.getUserByEmail);
 router.delete("/:id", checkLogin, userController.deleteUser);
 router.put("/:id", checkLogin, userController.updateUser);
 // router.post(
